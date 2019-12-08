61600c3f5cb3bf889d6f2261a202dac6066efa36
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-12-07 21:40:53 -0600

    wip

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var app = express()
 // Serve URLs like /ftp/thing as public/ftp/thing
 // The express.static serves the file contents
 // The serveIndex is this module serving the directory
-app.use('/', express.static('/'), serveIndex('assets', {'icons': true}))
+// app.use('/', express.static('/'), serveIndex('assets', {'icons': true}))
+app.use(serveStatic('public/ftp', { 'index': ['default.html', 'default.htm'] }))
+
 
 // Listen
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
