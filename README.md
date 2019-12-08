f7dcd6665fbe849328475fce67c600b24e9a4693
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-12-07 20:56:45 -0600

    started work

---
+++ README.md
@@ -0,0 +1,3 @@
+# cs315-lecture-notes
+
+This is repo of the lecture notes I made when I was a TA for cs315 between 15-17.
---
+++ index.js
@@ -0,0 +1,14 @@
+var express    = require('express')
+var serveIndex = require('serve-index')
+const port = process.env.PORT || 3000
+
+var app = express()
+
+// Serve URLs like /ftp/thing as public/ftp/thing
+// The express.static serves the file contents
+// The serveIndex is this module serving the directory
+app.use('/', express.static('/'), serveIndex('assets', {'icons': true}))
+
+// Listen
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
